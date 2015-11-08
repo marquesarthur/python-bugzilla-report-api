7e970d02c926ae22a404ab984712835b0d052173
marquesarthur <marques.art@gmail.com>
2015-11-08 13:18:44 -0300

    Bugs per assignee prototype

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class BugzillaReport:
     self.uri = uri + REST
     self.loggedIn = False
     self.loginInfo = None
-    self.bugs = { 'opened' : [], 'closed' : [], 'assigned' : [], 'rejected' : [], 'resolved' : [] }
+    self.bugs = { 'opened' : [], 'closed' : [], 'assigned' : [], 'rejected' : [], 'resolved' : [], 'all' : [] }
 
   def isLoggedIn(self):
     """ Verify if the user is logged in """
@@ -41,7 +41,7 @@ class BugzillaReport:
 
   def getIncludedFields(self):
     """ Get the list of fields to be extracted from the bug info """
-    return '&include_fields=id,component,classification,creation_time,last_change_time,is_open,priority,severity,status,summary'
+    return '&include_fields=id,component,classification,creation_time,last_change_time,is_open,priority,severity,status,summary,assigned_to,resolution'
 
   def login(self):
     """ Login into the bugzilla server """
@@ -76,7 +76,9 @@ class BugzillaReport:
     print '>>> Getting all [%s] bugs created from [%s]' %(status, dateParam)
     self.isLoggedIn()
     bugUri = self.uri + '/bug?status=' + status + '&last_change_time=' + dateParam + self.getIncludedFields() + self.getTokenParam()
-    self.bugs['opened'].extend(self.getBugs(status, dateParam))
+    bugs = self.getBugs(status, dateParam)
+    self.bugs['opened'].extend(bugs)
+    self.bugs['all'].extend(bugs)
 
   def getClosedBugs(self):
     """ Get all cloed bugs in the last month """
@@ -84,7 +86,9 @@ class BugzillaReport:
     status = 'RESOLVED'
     dateParam = currentDate.strftime('%Y-%m-%d')
     print '>>> Getting all [%s] bugs closed from [%s]' %(status, dateParam)
-    self.bugs['closed'].extend(self.getBugs(status, dateParam))
+    bugs = self.getBugs(status, dateParam)
+    self.bugs['closed'].extend(bugs)
+    self.bugs['all'].extend(bugs)
 
   def getBugs(self, status, dateParam):
     """ Get bugs with a specific status in a date range """
@@ -96,7 +100,6 @@ class BugzillaReport:
     print '>>> %s Bugs retrieved' %(len(bugList['bugs']))
     #print json.dumps(bugList['bugs'], indent=4, sort_keys=True)
     return bugList['bugs']
-
 
   def countBugsOfDay(self, day, bugList):
     """ Count the number of bugs on a day """
@@ -147,13 +150,8 @@ class BugzillaReport:
     return sum(1 for bug in bugList if json.dumps(bug['component']).strip('"') == component and json.dumps(bug['severity']).strip('"') == severity)
 
   def viewBugsPerComponentOf(self, bugList):
-    """ 
-      
-    """
-    
     components = []
     severities = []
-    
     data = []
 
     for bug in bugList:
@@ -163,10 +161,6 @@ class BugzillaReport:
         components.append(bugComponent)
       if not bugSeverity in severities:
         severities.append(bugSeverity)
-        
-    print components
-    print severities
-    print ''
 
     count = 0
     for component in components:
@@ -182,35 +176,66 @@ class BugzillaReport:
         print ' %s %s' %(severity, numberOfBugsInSeverity)
         x.append(severity)
         y.append(numberOfBugsInSeverity)
-
-      
       
       trace = graph.Bar(x=x, y=y, name=component)
       data.append(trace)
 
-      count += 1
-      if count == 3:
-        break
-
-
-      
-    pprint(data)
     return data   
-
-
-
 
   def viewBugsPerComponent(self):
     """ 
       Generate a report of bugs per component. 
       Currently it only generates the bugs that are opened
     """
-    
-
-    
-    
     return self.viewBugsPerComponentOf(self.bugs['opened'])
 
+  def viewBugsPerAssigneeOf(self, bugList):
+    assignees = []
+    data = []
+    
+    # Building up the bug list per assignee
+    for bug in bugList:
+      assigned_to = json.dumps(bug['assigned_to']).strip('"')
+      bugSeverity = json.dumps(bug['severity']).strip('"')
+      bugStatus = json.dumps(bug['status']).strip('"')
+      bugResolution = json.dumps(bug['resolution']).strip('"')
+      bugId = json.dumps(bug['id']).strip('"')
+
+      if not assigned_to in assignees:
+        assignees.append(dict(name = assigned_to, bugs = [],))
+
+      currently_assinged_to = filter(lambda assignee: assignee['name'] == assigned_to, assignees)[0]
+      bugs = currently_assinged_to['bugs']
+      bugs.append(dict(bug_id=bugId, severity=bugSeverity, resolution=bugResolution, status= bugStatus))
+
+    # Mapping it to a pie-donut chart
+
+    bug_status = ['VERIFIED', 'CONFIRMED', 'RESOLVED']
+    bug_resolution = ['FIXED', 'DUPLICATE']
+
+    for assignee in assignees:      
+      bugs_confirmed = sum(1 for bug in assignee['bugs'] if bug['status'] == 'CONFIRMED')
+      bugs_resolved = sum(1 for bug in assignee['bugs'] if bug['status'] == 'RESOLVED')
+      bugs_verified = sum(1 for bug in assignee['bugs'] if bug['status'] == 'VERIFIED')
+
+      bugs_fixed = sum(1 for bug in assignee['bugs'] if bug['resolution'] == 'FIXED')
+      bugs_dublicated = sum(1 for bug in assignee['bugs'] if bug['resolution'] == 'DUPLICATE')
+
+      if not bugs_confirmed == 0 or not bugs_resolved == 0 or not bugs_verified == 0:
+        trace = graph.Bar(
+          x=bug_status, 
+          y=[bugs_verified, bugs_confirmed, bugs_resolved], 
+          name=assignee['name'][0 : assignee['name'].index('@')])
+        data.append(trace)
+
+    return data
+
+  def viewBugsPerAssignee(self):
+    """ 
+      Generate a report of bugs per component. 
+      Currently it only generates the bugs that are opened
+    """
+    return self.viewBugsPerAssigneeOf(self.bugs['all'])
 
 
 # Some methods for test, at leat while I am developing the tool
@@ -220,8 +245,10 @@ report.getOpenedBugs()
 report.getClosedBugs()
 # data=report.viewBugsPerDay()
 # plot_url = py.plot(data, filename='number-of-bugs-per-day')
-data = report.viewBugsPerComponent()
-layout = graph.Layout(barmode='group')
+#data = report.viewBugsPerComponent()
+#layout = graph.Layout(barmode='group')
+#fig = graph.Figure(data=data, layout=layout)
+#plot_url = py.plot(data, filename='number-of-bugs-per-component-per-severity')
 
-fig = graph.Figure(data=data, layout=layout)
-plot_url = py.plot(data, filename='number-of-bugs-per-component-per-severity')
+data = report.viewBugsPerAssignee()
+plot_url = py.plot(data, filename='bugs-per-assignee')
