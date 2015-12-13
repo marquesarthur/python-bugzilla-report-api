03adc08cc73ad397aee5484c6b51dd79a25cbe6c
marquesarthur <marques.art@gmail.com>
2015-12-13 16:19:12 -0300

    Adding report html template

---
+++ report/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var App = angular.module('report', [
+    'ngRoute'
+]);
+
+App.service('ReportService', function($scope, $http, $q) {
+    var self = this;
+});
+
+App.controller('ReportController', function($scope, $http, $q, ReportService) {
+
+    var self = this;
+});
